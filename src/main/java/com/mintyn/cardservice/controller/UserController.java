de99e38ca48b7e569f1c512c267259494745e9a5
obemeuche <obemeuchechi@gmail.com>
2024-01-10 00:21:08 +0100

    feat/implemented signup and login features to protect endpoints

---
+++ src/main/java/com/mintyn/cardservice/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.mintyn.cardservice.controller;
+
+import com.mintyn.cardservice.request.LogInRequest;
+import com.mintyn.cardservice.request.SignUpRequest;
+import com.mintyn.cardservice.service.LogInService;
+import com.mintyn.cardservice.service.SignUpService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+@Slf4j
+public class UserController {
+
+    private final SignUpService signUpService;
+
+    private final LogInService logInService;
+
+    @PostMapping("/api/v1/signup")
+    public ResponseEntity<?> signup(@RequestBody @Valid SignUpRequest signUpRequest) {
+        return signUpService.signUp(signUpRequest);
+    }
+
+    @PostMapping("/api/v1/signin")
+    public ResponseEntity<?> signin(@RequestBody LogInRequest logInRequest) {
+        return logInService.signIn(logInRequest);
+    }
+
+}
